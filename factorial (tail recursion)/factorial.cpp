03d7700318c6c48985887926a12a0b052fb36e51
Bruno Zabala <brunozabalap@gmail.com>
2024-08-17 12:06:40 -0600

    Tail recursion example (factorial)

---
+++ factorial (tail recursion)/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Complejidad O(n)
+int factorial_rec_lineal(int n, int a){
+    if(n == 0){
+        return a;
+    }
+
+    else{
+        return factorial_rec_lineal(n - 1, a * n);
+    }
+}
+
+
+int main(){
+    int num;
+
+    cin >> num;
+    cout << "El factorial de " << num << " es: " << factorial_rec_lineal(num, 1);
+
+    return 0;
+}
