048e370c59bf4806dbc242d6e3aaec5e2fbd2422
Bruno Zabala <brunozabalap@gmail.com>
2024-11-08 21:40:18 -0600

    Class progress

---
+++ grafos/grafos.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]){	
+	//	C++
+	string sline, line, aux;
+	int index;
+	int a, b, current = -1;
+
+	int **matrix;
+
+	int *start, *end; 
+
+	ifstream inFile("grafo.txt"); 	    //  input file stream
+	//ofstream outFile("salida2.txt");		//	output file stream	
+	
+	//	Verifica que los archivos se hayan abierto correctamente
+	int i;
+    int j;
+	int N;
+    int E;
+
+	if (inFile.is_open()){	
+        //	Lee liniea a linea
+		while(getline(inFile, sline)){	
+            if(i == 1){   
+                //cout << sline << endl;
+				line = sline;
+				index = line.find(" "); 
+				aux = line.substr(0, index);
+				N = stoi(aux);//lee cuantos nodos son
+
+				line = line.substr(index+1);
+				index = line.find(" ");
+
+				line = line.substr(index+1);
+				index = line.find(" ");
+				aux = line.substr(0, index);
+				E = stoi(aux);
+
+				cout << "N: "<< N << endl;
+				cout << "E: "<< E << endl;
+
+				matrix = (int **) malloc ( N * sizeof(int*) );
+
+				for (j = 0; j < N; j++){	 
+                    matrix[j] = (int *) malloc(N * sizeof(int));	
+                }
+
+				// int** matrix = new int*[rows];
+				// for (int i = 0; i < rows; ++i)
+   				// matrix[i] = new int[cols];
+
+				// start = (* int) malloc(N * sizeof(int));
+				// end = (* int) malloc(N * sizeof(int));
+
+			}
+
+			else if(i > 1){	
+                //cout << sline << endl;
+
+				line = sline;
+				index = line.find(" "); 
+				aux = line.substr(0, index);
+				a = stoi(aux);
+
+				line = line.substr(index+1);
+				index = line.find(" "); 
+				aux = line.substr(0, index);
+				b = stoi(aux);
+
+				cout << "a y b: "<< a << " " << b << endl;
+
+				//	asignar valores a la matriz
+				//	indicando que a es adyacente a b, y viceversa
+                matrix[a][b] = 1;
+                matrix[b][a] = 1;
+			}
+			
+			i++;
+		}
+	} 
+
+	inFile.close();
+	//outFile.close();
+
+    cout << "Matriz del grafo:" << endl;
+
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            cout << matrix[i][j] << "\t";
+        }
+
+        cout << endl;
+    }
+}
